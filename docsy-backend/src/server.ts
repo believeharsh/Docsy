ca55ca2969b77a6cb8a47d134056eba418135d44
Harsh Dahiya <onlybelieveharsh@gmail.com>
2025-10-25 14:10:05 +0530

    feat: express server created

---
+++ docsy-backend/src/server.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// middlewares
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// route
+app.get('/', (req: Request, res: Response) => {
+  res.send('🚀 Basic Express + TypeScript server is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
